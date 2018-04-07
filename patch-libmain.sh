f9ed4c0197f88ff3c567698e73064add69173c1a
LostGamer <lorbbbbb@gmail.com>
2018-04-07 15:59:00 +0000

    update patches for libmain

--- patch-libmain.sh
+++ patch-libmain.sh
@@ -2,3 +2,6 @@ sed -e s/gl_supportMapBuffer/gl_supportAssBuffer/ -i libs/armeabi-v7a/libmain.so
 sed -e s/gl_separatedepthstencil/gl_separatedicksstencil/ -i libs/armeabi-v7a/libmain.so
 sed -e s/gl_nodepthtexture/gl_nodepthvaginas/ -i libs/armeabi-v7a/libmain.so
 sed -e s/gl_blitmode/gl_blittits/ -i libs/armeabi-v7a/libmain.so
+sed -e 's/gl_dropmips/gl_droptits/g' -i libmain.so
+sed -e 's/mat_parallaxmap/mat_parallaxsex/g' -i libmain.so
+sed -e 's/mat_specular/mat_speiular/g' -i libmain.so
