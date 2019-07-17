fe3f5331bf95ea7be2743e30474c2aae1dcd35f2
nillerusr <nillerusr@gmail.com>
2019-07-17 11:04:11 +0000

    delete my shitty jni functions from tierhook

--- jni/src/tierhook/tierhook.c
+++ jni/src/tierhook/tierhook.c
@@ -6,10 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include "SDL_mouse_c.h"
-extern SDL_Window *Android_Window;
+
 
 void simple_hook( void *target, void *func )
 {
@@ -45,60 +42,6 @@ int DefaultSpewFunc( int type, char *msg );
 void SpewOutputFunc(int (*func)( int, char*));
 void *GetSpewOutputFunc();
 
-JNIEXPORT void JNICALL Java_org_libsdl_app_SDLSurface_SetMousePos(JNIEnv *env, jclass cls, jint x, jint y)
-{
-	FILE *f;
-	f=fopen("/sdcard/touch.txt","a+");
-	SDL_Mouse *mouse = SDL_GetMouse();
-	/*__android_log_print( ANDROID_LOG_INFO,"FAGGOT", "x:%d y:%d", x, y );
-	fprintf(f,"setx:%d sety:%d\n",x,y);
-	fprintf(f,"mouse->x=%d, mouse->y=%d\n",mouse->x, mouse->y);
-	fprintf(f,"mouse->last_x=%d, mouse->last_y=%d\n",mouse->last_x, mouse->last_y);
-	fprintf(f,"mouse->xdelta=%d, mouse->ydelta=%d\n",mouse->xdelta, mouse->ydelta);*/
-	mouse->x = x;
-        mouse->y = y;
-//	mouse->xdelta = 0;
-//	mouse->ydelta = 0;
-	fclose(f);
-//	SDL_SendMouseMotion(0, SDL_TOUCH_MOUSEID, 0, 540, 360);
-}
-
-JNIEXPORT void JNICALL Java_org_libsdl_app_SDLSurface_MoveMouse(JNIEnv *env, jclass cls, jint x, jint y)
-{
-
-    SDL_Mouse *mouse = SDL_GetMouse();
-/*    int xrel;
-    int yrel;
-
-    xrel = x - mouse->last_x;
-    yrel = y - mouse->last_y;
-
-    if (!xrel && !yrel)
-        return;
-   __android_log_print( ANDROID_LOG_INFO,"FAGGOT", "OH YEAH" );
-    mouse->x += xrel;
-    mouse->y += yrel;
-
-    mouse->xdelta += xrel;
-    mouse->ydelta += yrel;
-
-    mouse->last_x = x;
-    mouse->last_y = y;
-*/
-FILE *f;
-f=fopen("/sdcard/touch.txt","a+");
-fprintf(f,"setx:%d sety:%d\n",x,y);
-fprintf(f,"BEFORE:\n");
-fprintf(f,"mouse->x=%d, mouse->y=%d\n",mouse->x, mouse->y);
-        fprintf(f,"mouse->last_x=%d, mouse->last_y=%d\n",mouse->last_x, mouse->last_y);
-        fprintf(f,"mouse->xdelta=%d, mouse->ydelta=%d\n",mouse->xdelta, mouse->ydelta);
-SDL_SendMouseMotion(0, SDL_TOUCH_MOUSEID, 0, x, y);
-	fprintf(f,"AFTER:\n");
-        fprintf(f,"mouse->x=%d, mouse->y=%d\n",mouse->x, mouse->y);
-        fprintf(f,"mouse->last_x=%d, mouse->last_y=%d\n",mouse->last_x, mouse->last_y);
-        fprintf(f,"mouse->xdelta=%d, mouse->ydelta=%d\n",mouse->xdelta, mouse->ydelta);
-fclose(f);
-}
 int _NewSpewMessage( int type, const char *fmt, va_list ap )
 {
 	char log[1000];
