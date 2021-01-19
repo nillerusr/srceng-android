614cdd8895534bc87a9ed9ab0e6e51ffb1a638dc
nillerusr <nillerusr@gmail.com>
2021-01-19 21:45:31 +0300

    move all scripts to scripts folder

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "pm install -r bin/srceng-debug.apk"
-ssh 127.0.0.1 "bash -c \"source /etc/bash/bashrc;cd /root/projects/srceng-android/bin;chmod 777 srceng-debug.apk;pm install -r srceng-debug.apk\""
-
--- wrappers-deploy.sh
+++
@@ -1,7 +0,0 @@
-/mnt/data/ndk/android-ndk-r10d/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc 
---sysroot=/mnt/data/ndk/android-ndk-r10d/platforms/android-8/arch-arm/ -I. first.c -shared -o libs/armeabi-v7a/libfirst.so -Wl,--no-undefined 
--llog&&/mnt/data/ndk/android-ndk-r10d/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc 
---sysroot=/mnt/data/ndk/android-ndk-r10d/platforms/android-8/arch-arm/ -I. tierhook.c -shared -o libs/armeabi-v7a/libtierhook.so -ltier0 -L libs/armeabi-v7a/ -landroidwrapper -O0 
--fno-omit-frame-pointer -g -llog -lSDL2 -Wl,--no-undefined&&JAVA_HOME=/opt/icedtea-bin-6.1.13.9/ sh build-java.sh&&/mnt/data2/android-sdk-linux/platform-tools/adb install -r 
-bin/hl2-signed.apk
-
